a38a61b9fd083bd598725467568dffa9b78bb639
Akshay Chandrachood <amc400@txstate.edu>
2017-11-05 20:02:34 -0600

    functionality to check left dia & horizontal win for X

--- Board.h
+++ Board.h
@@ -1,6 +1,9 @@
 #include<iostream>
 #include <stdlib.h>
 #include <ctime>
+#include <string.h>
+#include <string>
+
 using namespace std;
 
 class Board{
@@ -9,11 +12,18 @@ private:
     const int rows = 6;
     const int columns = 7;
     char board_position[6][7];
+    string find_X = "XXXX";
+    string find_0 = "0000";
+
 public:
     Board();
     void display_board();
     void add_checker(char c, int row, int col);
     void select_position(char);
+    bool isLeft(string check, int x, int y, int count, string finds);
+    bool call_isLeft(char player);
+    bool isHorizontal(char player);
+    bool eachRow(int);
 };
 
 
@@ -56,5 +66,81 @@ void Board:: select_position(char ch){
     cout<<r<<" "<<c<<endl;
 }
 
+bool Board :: call_isLeft(char player){
+    string find;
+    if(player=='X'){
+        find = find_X;
+    }
+    else if(player=='0'){
+        find = find_0;
+    }
+
+    for(int i =0; i < 3; i++){
+            for(int j=0; j<4; j++){
+              if(isLeft("", i,  j, 4, find)){
+                return true;
+              }
+        }
+    }
+    return false;
+}
+
+bool Board :: isLeft(string check, int x, int y, int count, string finds){
+    char chk = finds[0];
+
+    if(check==finds){
+        return true;
+    }
+
+    else if(count > 0){
+        count--;
+        if(board_position[x][y] == finds[0]){
+            check += board_position[x][y];
+        }
+        return isLeft(check, x+1, y+1, count, finds);
+    }
+
+    else
+        return false;
+}
 
 
+
+bool Board::isHorizontal(char player){
+    bool status=false;
+    for(int i=0;i<rows;i++){
+        if(!status){
+            cout<<"checking "<<i<<" th row.."<<endl;
+            status = eachRow(i);
+            cout<<"status "<<status<<endl;
+        }
+        if(status)
+            break;
+    }
+    if(status){
+        return true;
+    }
+}
+
+bool Board::eachRow(int r){
+    int count = 0;
+    string check="";
+    for(int i = 0; i< columns;i++){
+        if(board_position[r][i] == 'X'){
+            count++;
+            check += board_position[r][i];
+        }
+        else{
+          count=0;
+        }
+        if(count == 4 && check=="XXXX"){
+           break;
+        }
+    }
+
+    if(count==4)
+        return true;
+
+    else
+        return false;
+}
--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include "Board.h"
+#include <string>
+
 using namespace std;
 
 int main()
@@ -15,12 +17,55 @@ int main()
         b1.select_position('0');
     }*/
 
+
     b1.add_checker('X',0,0);
-    b1.add_checker('X',1,1);
+    b1.add_checker('0',0,1);
+    b1.add_checker('X',0,2);
+    b1.add_checker('0',0,3);
+    b1.add_checker('X',0,4);
+    b1.add_checker('0',0,5);
+    b1.add_checker('X',0,6);
+
+    b1.add_checker('0',2,0);
+    b1.add_checker('0',2,1);
     b1.add_checker('X',2,2);
+    b1.add_checker('X',2,3);
+    b1.add_checker('X',2,4);
+    b1.add_checker('X',2,5);
+    b1.add_checker('0',2,6);
+
+
+    b1.add_checker('0',3,0);
+    b1.add_checker('0',3,1);
+    b1.add_checker('X',3,2);
     b1.add_checker('X',3,3);
+    b1.add_checker('X',3,4);
+    b1.add_checker('X',3,5);
+    b1.add_checker('0',3,6);
+
+
+    b1.add_checker('0',4,0);
+    b1.add_checker('0',4,1);
+    b1.add_checker('0',4,2);
+    b1.add_checker('X',4,3);
+    b1.add_checker('X',4,4);
+    b1.add_checker('X',4,5);
+    b1.add_checker('X',4,6);
+
+    b1.add_checker('0',5,0);
+    b1.add_checker('X',5,1);
+    b1.add_checker('X',5,2);
+    b1.add_checker('X',5,3);
+    b1.add_checker('X',5,4);
+    b1.add_checker('0',5,5);
+    b1.add_checker('X',5,6);
+
 
     b1.display_board();
-    cout << "Hello world!" << endl;
+
+    cout<<"is horizontal: "<<b1.isHorizontal('X')<<endl;
+
+    //b1.display_board();
+
     return 0;
 }
