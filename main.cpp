0ff16b8378d262bf37519548c92944c9ec8fdffa
Akshay Chandrachood <amc400@txstate.edu>
2017-11-07 02:18:51 -0600

    updated board func call_isLeft

--- main.cpp
+++ main.cpp
@@ -9,20 +9,20 @@ int main()
     srand(time(NULL));
 
     Board b1;
+   // b1.display_board();
+
+    b1.add_checker('X',0,1);
+    b1.add_checker('X',1,2);
+    b1.add_checker('0',2,5);
+    b1.add_checker('X',3,6);
+ //   b1.add_checker('X',4,6);
+//    b1.add_checker('0',5,5);
+
+
     b1.display_board();
 
-    b1.add_checker('X',3,2);
-    b1.add_checker('X',3,2);
-    b1.add_checker('X',3,2);
-    b1.add_checker('X',3,2);
-    b1.add_checker('X',3,2);
-    b1.add_checker('X',3,2);
-
-
-    b1.display_board();
-
-    cout<<endl<<endl<<"is Hori X : "<<b1.isVertical('X')<<endl;
-    cout<<endl<<endl<<"is Hori 0 : "<<b1.isVertical('0')<<endl;
+    cout<<endl<<endl<<"is Hori X : "<<b1.call_isLeft('X')<<endl;
+    cout<<endl<<endl<<"is Hori 0 : "<<b1.call_isLeft('0')<<endl;
 
     //b1.display_board();
 
